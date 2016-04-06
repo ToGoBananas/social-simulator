1a80bdcf638149239862b6645f9ebe969c760848
togobananas <endpoo@gmail.com>
2016-04-06 06:24:30 +0300

    urls fixes

--- social_simulator/sky_news/models.py
+++ social_simulator/sky_news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 from social_simulator.dashboard.models import Post
 
@@ -7,3 +8,7 @@ class SkyPost(Post):
     header = models.CharField(max_length=50)
     header_image = models.ImageField(upload_to='sky/')
     short_text = models.CharField(max_length=100)
+
+    @staticmethod
+    def get_url():
+        return reverse('sky_news:index')
